a60ef60cbdd7f3ecc49a0c0b5c636d01a4854c5f
Dávid Halász <skateman@skateman.eu>
2017-12-22 20:55:35 +0100

    Reimplemented using Celluloid::IO

--- lib/surro-gate/proxy_error.rb
+++
@@ -1,5 +0,0 @@
-module SurroGate
-  # This error is raised when at least one of the pushed sockets is already registered
-  class ProxyError < StandardError
-  end
-end
--- spec/surro-gate/proxy_spec.rb
+++ spec/surro-gate/proxy_spec.rb
@@ -1,236 +1,88 @@
 require 'spec_helper'
 
 describe SurroGate::Proxy do
-  let(:socket) { IO.pipe.first }
-  let(:left) { IO.pipe }
-  let(:right) { IO.pipe }
-  let(:str) { 'test' }
-  let(:block) { :foo.to_proc }
-
-  let(:selectors) { subject.instance_variable_get(:@selectors) }
-  let(:reader) { subject.instance_variable_get(:@reader) }
-  let(:writer) { subject.instance_variable_get(:@writer) }
-  let(:thread) { subject.instance_variable_get(:@thread) }
   let(:logger) { Logger.new('/dev/null') }
+  let(:pair) { ::UNIXSocket.pair(:DGRAM, 0) }
+  let(:sock_a) { pair.first }
+  let(:sock_b) { pair.last }
 
   subject { described_class.new(logger) }
 
-  describe '#push' do
-    it 'calls the proxy method with the two arguments' do
-      expect(subject).to receive(:proxy).with(left.first, right.last, nil)
-      subject.push(left.first, right.last)
-    end
-
-    it 'returns with its arguments' do
-      expect(subject.push(left.first, right.last)).to eq([left.first, right.last])
-    end
-
-    context 'block given' do
-      it 'passes it further to #proxy' do
-        expect(subject).to receive(:proxy).with(left.first, right.last, block)
-        subject.push(left.first, right.last, &block)
-      end
-    end
-
-    context 'socket already pushed to the proxy' do
-      before { subject.push(left.first, right.last) }
-
-      it 'raises an exception' do
-        expect { subject.push(socket, right.last) }.to raise_error(SurroGate::ProxyError)
-      end
-    end
-  end
-
-  describe '#wait' do
-    context 'internal thread is running' do
-      before { subject.send(:thread_start) }
-
-      it 'joins the internal thread' do
-        expect(thread).to receive(:join)
-        subject.wait
-      end
-    end
-
-    context 'internal thread is not running' do
-      it 'returns nil' do
-        expect(subject.wait).to be_nil
-      end
-    end
-  end
-
-  describe '#alive?' do
-    context 'internal thread is running' do
-      before { subject.send(:thread_start) }
-
-      it 'returns with true' do
-        expect(subject.alive?).to be_truthy
-      end
-    end
-
-    context 'internal thread is not running' do
-      it 'returns with false' do
-        expect(subject.alive?).to be_falsey
-      end
-    end
-  end
-
-  describe '#proxy' do
-    it 'registers sockets for reading' do
-      subject.send(:proxy, left.first, right.last)
-      selectors.each do |selector|
-        [left.first, right.last].each do |socket|
-          expect(selector.registered?(socket)).to be_truthy
-        end
-      end
-    end
-
-    it 'starts the internal thread' do
-      expect(subject).to receive(:thread_start)
-      subject.send(:proxy, left.first, right.last)
-    end
-
-    describe 'disconnect' do
-      before do
-        subject.send(:proxy, left.first, right.last)
-        left.first.close
-      end
-
-      it 'disconnects the other end' do
-        subject.wait
-        expect(right.last.closed?).to be_truthy
-      end
-
-      context 'with no remaining sockets' do
-        it 'kills the internal thread' do
-          subject.wait
-          expect(thread).to be_nil
-        end
-      end
-
-      context 'with remaining sockets' do
-        before { subject.send(:proxy, IO.pipe.first, IO.pipe.last) }
-
-        it 'keeps the internal thread running' do
-          expect(subject).not_to receive(:thread_stop)
-          sleep(0.1) # Give some time to the background thread
-          expect(thread.alive?).to be_truthy
-        end
-      end
-    end
-
-    describe 'monitor.value.call' do
-      let(:rmon) { reader.select.select { |monitor| monitor.io == left.first }.first }
-      let(:wmon) { writer.select.select { |monitor| monitor.io == right.last }.first }
-
-      before do
-        allow(subject).to receive(:thread_start)
-        subject.send(:proxy, left.first, right.last)
-        allow(wmon).to receive(:writable?).and_return(writable)
-        left.last.write(str)
-      end
-
-      context 'only read is possible' do
-        let(:writable) { false }
-
-        it 'not invokes transmit' do
-          expect(subject).not_to receive(:transmit)
-          rmon.value.call
-        end
-
-        it 'returns with nil' do
-          expect(rmon.value.call).to be_nil
-        end
-      end
-
-      context 'both read and write are possible' do
-        let(:writable) { true }
-
-        it 'invokes transmit' do
-          expect(subject).to receive(:transmit).with(left.first, right.last, nil)
-          rmon.value.call
-        end
-
-        it 'returns with not nil' do
-          expect(rmon.value.call).to eq(str.length)
-        end
-      end
-    end
-  end
-
-  describe '#transmit' do
-    it 'transmits from left to right' do
-      left.last.write(str)
-      len = subject.send(:transmit, left.first, right.last, nil)
-      expect(right.first.read(len)).to eq(str)
-    end
-
-    it 'cleans up both sockets' do
-      left.last.close
-      expect(subject).to receive(:cleanup).with(left.first, right.last)
-      subject.send(:transmit, left.first, right.last, nil)
-    end
-
-    context 'block given' do
-      it 'passes the block to #cleanup' do
-        left.last.close
-        expect(subject).to receive(:cleanup).with(left.first, right.last)
-        subject.send(:transmit, left.first, right.last, block)
-      end
-    end
-  end
-
   describe '#cleanup' do
-    before do
-      reader.register(socket, :r)
-      writer.register(socket, :w)
-    end
+    context 'no cleanup block passed' do
+      before { subject.send(:cleanup, *sockets) }
 
-    it 'closes and deregisters a socket' do
-      subject.send(:cleanup, socket)
+      context 'single socket passed' do
+        let(:sockets) { [sock_a] }
 
-      selectors.each do |selector|
-        expect(selector.registered?(socket)).to be_falsey
-      end
-      expect(socket.closed?).to be_truthy
-    end
-
-    context 'block given' do
-      it 'yields to the block' do
-        expect { |b| subject.send(:cleanup, socket, &b) }.to yield_control
-      end
-    end
-
-    context 'sockets remaining after cleanup' do
-      before do
-        reader.register(IO.pipe.first, :r)
+        it 'closes the passed socket' do
+          expect(sock_a.closed?).to be_truthy
+          expect(sock_b.closed?).to be_falsey
+        end
       end
 
-      it 'keeps the internal thread' do
-        expect(subject).not_to receive(:thread_stop)
-        subject.send(:cleanup, socket)
+      context 'multiple sockets passed' do
+        let(:sockets) { [sock_a, sock_b] }
+
+        it 'closes both sockets' do
+          expect(sock_a.closed?).to be_truthy
+          expect(sock_b.closed?).to be_truthy
+        end
       end
     end
 
-    context 'no sockets remaining after cleanup' do
-      it 'stops the internal thread' do
-        expect(subject).to receive(:thread_stop)
-        subject.send(:cleanup, socket)
+    context 'passed cleanup block' do
+      it 'calls the block upon completion' do
+        expect { |b| subject.send(:cleanup, *pair, &b) }.to yield_control
       end
     end
   end
 
-  describe '#reactor' do
-    before do
-      allow(subject).to receive(:thread_start)
-      subject.push(left.first, right.last)
-      left.last.write(str)
+  describe '#push' do
+    let(:sock_a) { ::UNIXSocket.pair(:DGRAM, 0) }
+    let(:sock_b) { ::UNIXSocket.pair(:DGRAM, 0) }
+
+    let(:a_test) { sock_a.first }
+    let(:a_proxy) { Celluloid::IO::UNIXSocket.new(sock_a.last) }
+    let(:b_test) { sock_b.first }
+    let(:b_proxy) { Celluloid::IO::UNIXSocket.new(sock_b.last) }
+
+    let(:str) { 'get schwifty' }
+
+    before { subject.push(a_proxy, b_proxy) }
+
+    describe 'data transmission between two sockets' do
+      shared_examples 'transmission' do
+        it 'transmits data' do
+          left.write_nonblock(str)
+          sleep(0.1) # give some time for the transmission
+          expect(right.read_nonblock(16)).to eq(str)
+        end
+      end
+
+      context 'a -> b' do
+        let(:left) { a_test }
+        let(:right) { b_test }
+
+        include_examples 'transmission'
+      end
+
+      context 'b -> a' do
+        let(:left) { b_test }
+        let(:right) { a_test }
+
+        include_examples 'transmission'
+      end
     end
 
-    it 'calls the monitor value' do
-      reader.select(0.1) do |m|
-        expect(m.value).to receive(:call)
+    context 'socket gets closed' do
+      before { a_proxy.close }
+
+      it 'its pair gets closed' do
+        b_test.write(str)
+        sleep(0.1) # give some time for the transmission
+        expect(b_proxy).to be_closed
       end
-      subject.send(:reactor)
     end
   end
 end
