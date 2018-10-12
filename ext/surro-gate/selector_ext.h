56ba70059a08f82ee5bb6e87f4f3f2fe9e8b7ec3
Dávid Halász <skateman@skateman.eu>
2018-10-12 19:45:10 +0200

    Do not use the GVL when calling epoll_wait

--- ext/surro-gate/selector_ext.h
+++ ext/surro-gate/selector_ext.h
@@ -3,11 +3,21 @@
 
 #include "ruby.h"
 #include "ruby/io.h"
+#include "ruby/thread.h"
 #include "stdlib.h"
 #include "sys/epoll.h"
 
 #define SOCK_PTR(X) RFILE(X)->fptr->fd
 #define IVAR_TRUE(X, Y) rb_iv_get(X, Y) == Qtrue
+
+struct epoll_wait_args {
+  unsigned int epfd;
+  struct epoll_event *events;
+  int maxevents;
+  int timeout;
+
+  int result;
+};
 
 static VALUE SurroGate_Selector_allocate(VALUE self);
 static VALUE SurroGate_Selector_initialize(VALUE self, VALUE logger);
