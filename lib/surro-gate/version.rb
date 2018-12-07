6098e7a20f251bc322b2d8bf05c81b9e119db4f4
Dávid Halász <skateman@skateman.eu>
2018-12-07 19:28:12 +0100

    Bump version to 1.0.3

--- lib/surro-gate/version.rb
+++ lib/surro-gate/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 module SurroGate
-  VERSION = '1.0.2'.freeze
+  VERSION = '1.0.3'.freeze
   HAVE_EXT = RUBY_PLATFORM =~ /linux/ && !defined?(JRUBY_VERSION) && !ENV['SURRO_GATE_NOEXT']
 end
