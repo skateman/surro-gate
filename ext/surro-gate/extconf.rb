4d48f8781f6de2ce92f1001474a94cc22d04e568
Dávid Halász <skateman@skateman.eu>
2018-12-07 20:20:26 +0100

    Don't allow makefile creation on OSX

--- ext/surro-gate/extconf.rb
+++ ext/surro-gate/extconf.rb
@@ -1,4 +1,6 @@
 require 'mkmf'
 
-have_header 'sys/epoll.h'
-create_makefile 'surro-gate/selector_ext'
+if RUBY_PLATFORM =~ /linux/ && !defined?(JRUBY_VERSION) && !ENV['SURRO_GATE_NOEXT']
+  have_header 'sys/epoll.h'
+  create_makefile 'surro-gate/selector_ext'
+end
